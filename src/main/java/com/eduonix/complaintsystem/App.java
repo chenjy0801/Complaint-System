eada5ce7568cf54b032a6bb05c956aaf3b33dc1e
chenjy0801 <jc4805@columbia.edu>
2018-11-24 21:36:44 -0500

    basic configuration

---
+++ src/main/java/com/eduonix/complaintsystem/App.java
@@ -0,0 +1,13 @@
+package com.eduonix.complaintsystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
