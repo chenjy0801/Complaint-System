cfebd5324fdc5a902c12d53697641a9d40a6e91e
chenjy0801 <jc4805@columbia.edu>
2018-11-24 22:40:58 -0500

    views

--- src/main/java/com/eduonix/complaintsystem/App.java
+++ src/main/java/com/eduonix/complaintsystem/App.java
@@ -1,13 +1,30 @@
 package com.eduonix.complaintsystem;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class App {
+	
+	@RequestMapping("/fileComplaint")
+	public String fileComplaint() {
+		return "fileComplaint";
+	}
+	
+	@RequestMapping("/submitComplaint")
+	public String submitComplaint() {
+		return "submitComplaint";
+	}
+	
+	@RequestMapping(name="/showComplaints", method=RequestMethod.GET)
+	public String showComplaints() {
+		return "showEnterPassword";
+	}
+	
+	@RequestMapping(name="/showComplaints", method=RequestMethod.POST)
+	public String showComplaintsPost() {
+		return "showComplaints";
+	}
+
 }
